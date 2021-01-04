be6dbac9a7f672c4e9d2fd5100469abd1d352565
Raphael Portela <rafaeljansenaraujo@gmail.com>
2021-01-04 20:01:17 -0300

    eslint config, prettier e globalstyles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,15 @@
-import type { AppProps } from "next/app";
+import { AppProps } from 'next/app'
+import { ThemeProvider } from 'styled-components'
+import GlobalStyle from '../styles/GlobalStyle'
+import theme from '../styles/theme'
 
-export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
+
+export default MyApp
---
+++ src/styles/theme.ts
@@ -0,0 +1,9 @@
+const theme = {
+  colors: {
+    background: '#E9EEEF',
+
+    primary: '#8257e6'
+  }
+}
+
+export default theme

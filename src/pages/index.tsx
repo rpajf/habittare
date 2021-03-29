5fdc7d2d4018d818eb7a9b4f130506069cf5ee76
Arthur Araujo <artharaujo234@gmail.com>
2021-03-29 18:47:38 -0300

    fix: connect to database with nextConnect on index and single property page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -21,14 +21,25 @@ import {
   getLatestProperties
 } from '@/services/properties'
 
-export const getServerSideProps: GetServerSideProps = async () => {
-  const sliderProperties = await getHomePageProperties()
-  const latestProperties = await getLatestProperties()
+import nextConnect from 'next-connect'
+import databaseMiddleware from '@/middlewares/db'
 
-  return {
-    props: {
-      sliderProperties: JSON.parse(JSON.stringify(sliderProperties)),
-      latestProperties: JSON.parse(JSON.stringify(latestProperties))
+export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
+  const handler = nextConnect().use(databaseMiddleware)
+  try {
+    await handler.run(req, res)
+    const sliderProperties = await getHomePageProperties()
+    const latestProperties = await getLatestProperties()
+    return {
+      props: {
+        sliderProperties: JSON.parse(JSON.stringify(sliderProperties)),
+        latestProperties: JSON.parse(JSON.stringify(latestProperties))
+      }
+    }
+  } catch (error) {
+    console.log(error)
+    return {
+      props: { sliderProperties: [], latestProperties: [] }
     }
   }
 }

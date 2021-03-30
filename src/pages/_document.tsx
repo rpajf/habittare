be507c5467a3543bdf8df80dae5a18804fcc2386
Arthur Araujo <artharaujo234@gmail.com>
2021-03-30 16:33:09 -0300

    fix: fix import on poppins font

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -47,6 +47,10 @@ export default class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap"
             rel="stylesheet"
           />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500&display=swap"
+            rel="stylesheet"
+          />
           {/* <link rel="icon" href=""/> */}
         </Head>
         <body>
--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -1,8 +1,6 @@
 import { createGlobalStyle } from 'styled-components'
 
 export default createGlobalStyle`
-@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500&display=swap');
-
   * {
     margin:0;
     padding:0;

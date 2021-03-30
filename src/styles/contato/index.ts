b559275f4b239a6ddfd335af4d501d3ff5bb5a7b
rpajf <rafaeljansenaraujo@gmail.com>
2021-03-30 16:24:38 -0300

    Contact responsive ajusts

--- src/styles/contato/index.ts
+++ src/styles/contato/index.ts
@@ -1,16 +1,14 @@
 import styled from 'styled-components'
 
-export const Container = styled.div``
+// export const Container = styled.div``
 
-export const Content = styled.div`
+export const Container = styled.div`
   display: flex;
-  @media (min-width: 310px) and (max-width: 480px) {
-    align-items: center;
-  }
-  justify-content: space-around;
+  flex-direction: column;
+  align-items: center;
   @media (min-width: 310px) and (max-width: 480px) {
     display: flex;
-    flex-direction: column-reverse;
+    flex-direction: column;
     align-items: center;
     margin: 0 15px 0 15px;
   }
@@ -18,13 +16,12 @@ export const Content = styled.div`
 export const TextWrapper = styled.div`
   display: flex;
   flex-direction: column;
-  max-width: 600px;
   margin: 50px auto;
-  text-align: center;
+  justify-content: center;
+
   @media (min-width: 310px) and (max-width: 480px) {
     align-items: center;
     justify-content: center;
-    margin: 0;
   }
 `
 
@@ -32,13 +29,14 @@ export const Title = styled.text`
   font-style: normal;
   font-weight: 500;
   font-size: 32px;
+  text-align: center;
   line-height: 54px;
 
   color: ${props => props.theme.colors.primary};
   @media (min-width: 310px) and (max-width: 480px) {
-    font-size: 24px;
+    font-size: 20px;
     text-align: center;
-    line-height: 36px;
+    line-height: 32px;
   }
 `
 
@@ -47,7 +45,6 @@ export const ContentText = styled.text`
   font-weight: normal;
   font-size: 18px;
   line-height: 27px;
-
   color: #000000;
 `
 export const SubTitle = styled.text`
@@ -59,6 +56,7 @@ export const SubTitle = styled.text`
   color: ${props => props.theme.colors.primary};
   @media (min-width: 310px) and (max-width: 480px) {
     font-size: 18px;
+    line-height: 26px;
   }
 `
 
@@ -66,10 +64,13 @@ export const SideTittle = styled.text`
   font-weight: 500;
   font-size: 24px;
   line-height: 36px;
+
   color: ${props => props.theme.colors.primary};
   margin-bottom: 15px;
   @media (min-width: 310px) and (max-width: 480px) {
-    font-size: 20px;
+    text-align: center;
+    font-size: 16px;
+    line-height: 24px;
   }
 `
 export const FormWrapper = styled.div`
@@ -86,6 +87,9 @@ export const SideTextWrapper = styled.div`
   display: flex;
   flex-direction: column;
   max-width: 320px;
+  @media (min-width: 310px) and (max-width: 480px) {
+    margin-bottom: 30px;
+  }
 `
 export const SideContent = styled.text`
   font-size: 16px;
@@ -93,5 +97,6 @@ export const SideContent = styled.text`
   line-height: 24px;
   @media (min-width: 310px) and (max-width: 480px) {
     font-size: 12px;
+    text-align: center;
   }
 `
